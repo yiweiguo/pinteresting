e631839c7913af542a7b414d3e8e3a4201a80827
Paul Kuo <yiweiguo0301@gmail.com>
2016-08-03 14:30:04 -0400

    added Bootstrap a d customerization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about"
+  get "about" => "pages#about" #create an about path
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
