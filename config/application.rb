d7ebd2c34ace27fdd74fa24b347cd1e52063cc57
Paul Kuo <yiweiguo0301@gmail.com>
2016-08-10 20:01:55 -0400

    pincontrol

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Pinteresting
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #config.assests.initialize_on_precompile = false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803195204) do
+ActiveRecord::Schema.define(version: 20160808200446) do
+
+  create_table "pins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
