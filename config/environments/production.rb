b929bba62e15866b44f67bacfb08f0e17d64f0eb
Paul Kuo <yiweiguo0301@gmail.com>
2016-08-13 13:13:11 -0400

    bucket name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   :storage => :s3,
   :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
+    :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
