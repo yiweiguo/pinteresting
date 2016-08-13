bf6238f8bc9f37d3972a2212af3c0ad94f975488
Paul Kuo <yiweiguo0301@gmail.com>
2016-08-13 13:09:18 -0400

    gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,9 @@ Rails.application.configure do
   #sets paper clip to upload images to s3
   config.paperclip_defaults = {
   :storage => :s3,
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
+    :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
