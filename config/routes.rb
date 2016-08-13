cb22f939a5d5fb7555b919ce7ea2aedfc5bd7c1c
Paul Kuo <yiweiguo0301@gmail.com>
2016-08-13 16:32:21 -0400

    Clean up the page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :pins
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about" #create an about path
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
